71f0da385c8f09b1fa2f91ff643bdc2943c1be1e
pratik-tagline <pratikm.tagline@gmail.com>
2022-06-06 16:15:18 +0530

    project set up

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
+import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
+import { CounterComponent } from './counter/counter/counter.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CounterComponent,
+    CounterButtonsComponent,
+    CounterOutputComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
